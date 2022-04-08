506c18a547f51533a7ab368d1e4d5010702d1247
Vitor <moutimg@gmail.com>
2022-04-08 18:22:04 -0300

    Updates

---
+++ precache-manifest.dd5d31dce91e2bbcb822816566fd0eda.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb12460d809a76325be4a22ddb74d773",
+    "url": "/game-trivia-repo/index.html"
+  },
+  {
+    "revision": "9a4e41922d309c52b383",
+    "url": "/game-trivia-repo/static/css/main.7e702091.chunk.css"
+  },
+  {
+    "revision": "f235b18b59ddc7dd0af4",
+    "url": "/game-trivia-repo/static/js/2.d4161e73.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/game-trivia-repo/static/js/2.d4161e73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a4e41922d309c52b383",
+    "url": "/game-trivia-repo/static/js/main.5465d4a2.chunk.js"
+  },
+  {
+    "revision": "2d4e20eeda22ad916ac6",
+    "url": "/game-trivia-repo/static/js/runtime-main.65fe69db.js"
+  },
+  {
+    "revision": "7d6c26b43641ce2c1a80bc6f56435e4a",
+    "url": "/game-trivia-repo/static/media/hourglass.7d6c26b4.png"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/game-trivia-repo/static/media/trivia.466d153e.png"
+  }
+]);
